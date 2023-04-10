a698cdc077655b12b0dded0eff8dc6f9472a9e0b
Adebiyi Blessing <yoyoplenty@gmail.com>
2023-04-10 21:26:29 +0100

    completed register UI

--- src/pages/onboarding/register.tsx
+++ src/pages/onboarding/register.tsx
@@ -1,13 +1,25 @@
 import * as Yup from "yup";
 import { Formik } from "formik";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FormInput, FullButton } from "../../components";
-import { getAsset } from "../../utils/helper";
+import { convertImageToBase64, getAsset } from "../../utils/helper";
 
 const Register = () => {
+  const [file, setFile] = useState<any>(null);
+
   const schema = Yup.object({
+    email: Yup.string().required(),
     username: Yup.string().required(),
+    password: Yup.string().required(),
   });
+
+  /**Formik Needs to support file upload */
+  const onUpload = async (e) => {
+    const base64 = await convertImageToBase64(e.target.files[0]);
+
+    setFile(base64);
+  };
 
   return (
     <div className="container-fluid m-0 bg-img">
@@ -18,30 +30,35 @@ const Register = () => {
             <p>We are Happy to Have you on Board</p>
           </div>
 
-          <div className="text-secondary my-2 mb-lg-4 px-lg-4 p-2">
+          <div className="text-secondary px-lg-3 p-2">
             <Formik
-              initialValues={{ username: "" }}
+              initialValues={{ username: "", email: "", password: "" }}
               validationSchema={schema}
-              onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                  setSubmitting(false);
-                }, 400);
+              onSubmit={async (values) => {
+                values = await Object.assign(values, { profile: file || "" });
+
+                console.log(values);
               }}
             >
               {(formik) => (
                 <form onSubmit={formik.handleSubmit}>
-                  <div className="profile flex justify-center py-lg-4 py-1">
-                    <img src={getAsset("profile.png")} className="profile-img img-thumbnail" alt="avatar" />
+                  <div className="profile flex justify-center py-lg-2 py-1">
+                    <label htmlFor="profile">
+                      <img src={file || getAsset("avatar.jpeg")} className="profile-img img-thumbnail" alt="avatar" />
+                    </label>
+                    <input type="file" onChange={onUpload} id="profile" name="profile" />
                   </div>
+
+                  <FormInput id="email" />
                   <FormInput id="username" />
+                  <FormInput id="password" type="password " />
 
                   <FullButton text="Let's Go" />
-                  <div className="d-flex justify-content-center p-lg-2 mb-lg-5 mb-2">
+                  <div className="d-flex justify-content-center p-lg-2 mb-lg-3 mb-2">
                     <p className="nav-link p-lg-1">Already Registered?</p>
 
                     <Link className="nav-link text-danger p-lg-1" to="/login">
-                      Login
+                      Login Now
                     </Link>
                   </div>
                 </form>
--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,7 +1,18 @@
-export function getAsset(asset: string) {
+export const getAsset = (asset: string) => {
   return require(`../assets/${asset}`);
-}
+};
 
 export const errorParser = (errors: any, touched: any, key: any) => {
   return errors[key] && touched[key] && errors[key];
+};
+
+/**Convert Image to base 64 */
+export const convertImageToBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => resolve(fileReader.result);
+    fileReader.onerror = (error) => reject(error);
+  });
 };
